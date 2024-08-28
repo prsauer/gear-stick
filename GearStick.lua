f465856385c673bbea680513be727f591d0dc8e4
Paul Sauer <paulrsauer@gmail.com>
2024-08-28 17:20:38 -0400

    Updates

--- GearStick.lua
+++ GearStick.lua
@@ -1,128 +1,51 @@
 
-local function createTooltipHandler(accessor, getterName)
-	local ttfunc = function(tip, ...)
-		local itemID = nil
+local function writeTooltip(tooltip, itemID, currentSpecId)
+	local key = currentSpecId .. itemID
+	local itemInventoryType = C_Item.GetItemInventoryTypeByID(itemID)
 
-		local currentSpec = GetSpecialization()
-		local currentSpecId = currentSpec and select(1, GetSpecializationInfo(currentSpec)) or "None"
-
-
-		local tooltipData = C_TooltipInfo[getterName](...)
-
-		if (tooltipData ~= nil and tooltipData['type'] == 0 and tooltipData['id'] ~= nil) then
-			itemID = tooltipData['id']
+	if GearStickSettings["2v2"] then
+		if usageDb2v2[key] then
+			tooltip:AddLine("[2v2]: |cFF11FF00" .. usageDb2v2[key][1] .. "%|r players use this" .. (usageDb2v2[key][2] and " (bis)" or ""), 0.90, 0.80, 0.60,  0);
+			if usageDb2v2[key][3] ~= "" and GearStickSettings["bis"] then
+				tooltip:AddLine("[2v2-bis]: " .. usageDb2v2[key][3], 0.90, 0.80, 0.60,  0);
+			end
+		else
+			if GearStickSettings["bis"] and itemInventoryType and usageDb2v2[currentSpecId .. itemInventoryType] then
+				tooltip:AddLine("[2v2-bis]: " .. usageDb2v2[currentSpecId .. itemInventoryType][3], 0.90, 0.80, 0.60,  0);
+			end
 		end
-
-		if itemID then
-            -- ["250202459"] = "|cFF11FF0071.4%|r players use this (bis)",
-			local key = currentSpecId .. itemID
-			if GearStickSettings["2v2"] then
-				if usageDb2v2[key] then
-					GameTooltip:AddLine("[2v2]: |cFF11FF00" .. usageDb2v2[key][1] .. "%|r players use this" .. (usageDb2v2[key][2] and " (bis)" or ""), 0.90, 0.80, 0.60,  0);
-					if usageDb2v2[key][3] ~= "" and GearStickSettings["bis"] then
-						GameTooltip:AddLine("[2v2-bis]: " .. usageDb2v2[key][3], 0.90, 0.80, 0.60,  0);
-					end
-				else
-					if GearStickSettings["bis"] and itemInventoryType and usageDb2v2[currentSpecId .. itemInventoryType] then
-						itemInventoryType = C_Item.GetItemInventoryTypeByID(itemID)
-						GameTooltip:AddLine("[2v2-bis]: " .. usageDb2v2[currentSpecId .. itemInventoryType][3], 0.90, 0.80, 0.60,  0);
-					end
-				end
-			end
-
-			if GearStickSettings["3v3"] then
-				if usageDb3v3[key] then
-					GameTooltip:AddLine("[3v3]: |cFF11FF00" .. usageDb3v3[key][1] .. "%|r players use this" .. (usageDb3v3[key][2] and " (bis)" or ""), 0.90, 0.80, 0.60,  0);
-					if usageDb3v3[key][3] ~= "" and GearStickSettings["bis"] then
-						GameTooltip:AddLine("[3v3-bis]: " .. usageDb3v3[key][3], 0.90, 0.80, 0.60,  0);
-					end
-				else
-					if GearStickSettings["bis"] and itemInventoryType and usageDb3v3[currentSpecId .. itemInventoryType] then
-						itemInventoryType = C_Item.GetItemInventoryTypeByID(itemID)
-						GameTooltip:AddLine("[3v3-bis]: " .. usageDb3v3[currentSpecId .. itemInventoryType][3], 0.90, 0.80, 0.60,  0);
-					end
-				end
-			end
-
-			if GearStickSettings["pve"] then
-				if usageDbPvE[key] then
-					GameTooltip:AddLine("[PvE]: |cFF11FF00" .. usageDbPvE[key][1] .. "%|r players use this" .. (usageDbPvE[key][2] and " (bis)" or ""), 0.90, 0.80, 0.60,  0);
-					if usageDbPvE[key][3] ~= "" and GearStickSettings["bis"] then
-						GameTooltip:AddLine("[PvE-bis]: " .. usageDbPvE[key][3], 0.90, 0.80, 0.60,  0);
-					end
-				else
-					if GearStickSettings["bis"] and itemInventoryType and usageDbPvE[currentSpecId .. itemInventoryType] then
-						itemInventoryType = C_Item.GetItemInventoryTypeByID(itemID)
-						GameTooltip:AddLine("[PvE-bis]: " .. usageDbPvE[currentSpecId .. itemInventoryType][3], 0.90, 0.80, 0.60,  0);
-					end
-				end
-			end
-			-- if usageDb[itemID]["SoloShuffle"] then
-			-- 	GameTooltip:AddLine(usageDb[itemID]["SoloShuffle"], 0.90, 0.80, 0.60,  0)
-			-- end
-			-- if usageDb[itemID]["PvE"] then
-			-- 	GameTooltip:AddLine(usageDb[itemID]["PvE"], 0.90, 0.80, 0.60,  0)
-			-- end
-            if GearStickSettings["debug"] then
-                GameTooltip:AddLine("GT.ItemID: " .. itemID, 1, 0.3, 0.3);
-				GameTooltip:AddLine("GT.SpecId: " .. currentSpecId, 1, 0.3, 0.3);
-            end
-		end
-	
-        if GearStickSettings["debug"] then
-		    GameTooltip:AddLine("GT.TTHook: ".. accessor, 1, 0.3, 0.3);
-        end
-		GameTooltip:Show();
 	end
-	return ttfunc
-end
 
--- See ln#355 in TooltipDataHandler.lua for more context and a list of all TT handlers and their accessors
-do
-	local accessors = {
-		SetMerchantItem = "GetMerchantItem",
-		SetItemByID = "GetItemByID",
-		SetInventoryItem = "GetInventoryItem",
-		SetRecipeReagentItem = "GetRecipeReagentItem",
-		SetWeeklyReward = "GetWeeklyReward",
-		SetVoidItem = "GetVoidItem",
-		SetVoidDepositItem = "GetVoidDepositItem",
-		SetVoidWithdrawalItem = "GetVoidWithdrawalItem",
-		SetInboxItem = "GetInboxItem",
-		SetSendMailItem = "GetSendMailItem",
-		SetTradePlayerItem = "GetTradePlayerItem",
-		SetTradeTargetItem = "GetTradeTargetItem",
-		SetQuestItem = "GetQuestItem",
-		SetQuestLogItem = "GetQuestLogItem",
-		SetQuestLogSpecialItem = "GetQuestLogSpecialItem",
-		SetLootItem = "GetLootItem",
-		SetLootRollItem = "GetLootRollItem",
-		SetGuildBankItem = "GetGuildBankItem",
-		SetHeirloomByItemID = "GetHeirloomByItemID",
-		SetRuneforgeResultItem = "GetRuneforgeResultItem",
-		SetTransmogrifyItem = "GetTransmogrifyItem",
-		SetArtifactItem = "GetArtifactItem",
-		SetBagItem = "GetBagItem",
-		SetBagItemChild = "GetBagItemChild",
-		SetBuybackItem = "GetBuybackItem",
-		SetInventoryItemByID = "GetInventoryItemByID",
-		SetItemKey = "GetItemKey",
-		SetLFGDungeonReward = "GetLFGDungeonReward",
-		SetLFGDungeonShortageReward = "GetLFGDungeonShortageReward",
-		SetUpgradeItem = "GetUpgradeItem",
-		SetEquipmentSet = "GetEquipmentSet",
-		SetMerchantCostItem = "GetMerchantCostItem",
-		SetRecipeResultItem = "GetRecipeResultItem",
-		SetRecipeResultItemForOrder = "GetRecipeResultItemForOrder",
-		SetOwnedItemByID = "GetOwnedItemByID",
-		SetHyperlink = "GetHyperlink",
-		SetItemInteractionItem = "GetItemInteractionItem",
-		SetItemByGUID = "GetItemByGUID",
-	};
+	if GearStickSettings["3v3"] then
+		if usageDb3v3[key] then
+			tooltip:AddLine("[3v3]: |cFF11FF00" .. usageDb3v3[key][1] .. "%|r players use this" .. (usageDb3v3[key][2] and " (bis)" or ""), 0.90, 0.80, 0.60,  0);
+			if usageDb3v3[key][3] ~= "" and GearStickSettings["bis"] then
+				tooltip:AddLine("[3v3-bis]: " .. usageDb3v3[key][3], 0.90, 0.80, 0.60,  0);
+			end
+		else
+			if GearStickSettings["bis"] and itemInventoryType and usageDb3v3[currentSpecId .. itemInventoryType] then
+				tooltip:AddLine("[3v3-bis]: " .. usageDb3v3[currentSpecId .. itemInventoryType][3], 0.90, 0.80, 0.60,  0);
+			end
+		end
+	end
 
-	local handler = TooltipDataHandlerMixin;
-	for accessor, getterName in pairs(accessors) do
-		hooksecurefunc(GameTooltip, accessor, createTooltipHandler(accessor, getterName));
+	if GearStickSettings["pve"] then
+		if usageDbPvE[key] then
+			tooltip:AddLine("[PvE]: |cFF11FF00" .. usageDbPvE[key][1] .. "%|r players use this" .. (usageDbPvE[key][2] and " (bis)" or ""), 0.90, 0.80, 0.60,  0);
+			if usageDbPvE[key][3] ~= "" and GearStickSettings["bis"] then
+				tooltip:AddLine("[PvE-bis]: " .. usageDbPvE[key][3], 0.90, 0.80, 0.60,  0);
+			end
+		else
+			if GearStickSettings["bis"] and itemInventoryType and usageDbPvE[currentSpecId .. itemInventoryType] then
+				tooltip:AddLine("[PvE-bis]: " .. usageDbPvE[currentSpecId .. itemInventoryType][3], 0.90, 0.80, 0.60,  0);
+			end
+		end
+	end
+
+	if GearStickSettings["debug"] then
+		tooltip:AddLine("GT.ItemID: " .. itemID, 1, 0.3, 0.3);
+		tooltip:AddLine("GT.itemInventoryType: " .. itemInventoryType, 1, 0.3, 0.3);
+		tooltip:AddLine("GT.SpecId: " .. currentSpecId, 1, 0.3, 0.3);
 	end
 end
 
@@ -137,7 +60,25 @@ function frame:OnEvent(event, arg1)
 	end
 end
 
+function GST_OnTooltipSetItem(tooltip, tooltipData)
+
+	local currentSpec = GetSpecialization()
+	local currentSpecId = currentSpec and select(1, GetSpecializationInfo(currentSpec)) or "None"
+
+	local itemID = nil
+	if (tooltipData ~= nil and tooltipData['type'] == 0 and tooltipData['id'] ~= nil) then
+		itemID = tooltipData['id']
+	end
+
+	if (itemID) then
+		writeTooltip(tooltip, itemID, currentSpecId)
+	end
+
+	tooltip:Show();
+end
+
 frame:SetScript("OnEvent", frame.OnEvent);
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, GST_OnTooltipSetItem)
 
 SlashCmdList.GST = function(arg1)
 	if arg1 == nil or arg1 == "" then
